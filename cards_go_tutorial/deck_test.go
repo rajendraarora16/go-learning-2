a4eda748fcf15fd85f954525eadc39d9fa607e70
rajarora-taboola <rajendra.a@taboola.com>
2021-12-17 15:45:28 +0700

    Go learning v2

---
+++ cards_go_tutorial/deck_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 16 {
+		t.Errorf("Expected the length 16 but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Something went wrong at last position of slice array: %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards index but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
+}
---
+++ structs_go/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName string
+	contactInfo
+}
+
+
+func main() {
+	jim := person {
+		firstName: "rajendra",
+		lastName: "arora",
+		contactInfo: contactInfo {
+			email: "rajendra@gmail.com",
+			zipCode: 324007,
+		},
+	}
+
+	jimPointer := &jim
+	
+	jimPointer.updateName("Jimmy")
+	jim.print()
+}
+
+func (pointerToPerson *person) updateName(firstNewName string) {
+	(*pointerToPerson).firstName = firstNewName
+}
+
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
